6241ee433fa748f70a9f04bb2662d6b2e9137d18
devchea <rchea817@gmail.com>
2021-03-06 09:09:49 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+const page_url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States'
+
+const getStockStatus = async() => {
+    try {
+        const { data } = await axios.get(
+            page_url
+        )
+        console.log(data);
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+
+getStockStatus()
